966bdbaf4ef573569572053b439f7f06b6908658
Alexey Avramenko <2822698@gmail.com>
2017-12-15 13:52:48 +0700

    export install function

--- src/event-bus.js
+++ src/event-bus.js
@@ -1,14 +1,12 @@
-export default {
-    install(Vue) {
-        Vue.mixin({
-            beforeCreate: function() {
-                const options = this.$options
-                if (options.bus) {
-                  this.$bus = options.bus; 
-                } else if (options.parent && options.parent.$bus) {
-                  this.$bus = options.parent.$bus
-                }
+export function install(Vue) {
+    Vue.mixin({
+        beforeCreate: function() {
+            const options = this.$options
+            if (options.bus) {
+                this.$bus = options.bus; 
+            } else if (options.parent && options.parent.$bus) {
+                this.$bus = options.parent.$bus
             }
-        })
-    }
+        }
+    })
 }
